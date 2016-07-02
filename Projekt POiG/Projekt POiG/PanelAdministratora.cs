78919771f315ad873d7ae3d94cc184762f4ac955
Aleksander Imbierowski <alek940@gmail.com>
2016-07-02 11:44:29 +0200

    Wypełnienie informacji o ksiązkach

--- Projekt POiG/Projekt POiG/PanelAdministratora.cs
+++ Projekt POiG/Projekt POiG/PanelAdministratora.cs
@@ -16,6 +16,24 @@ namespace Projekt_POiG
         {
             InitializeComponent();
             fill_comboBox1();
+             fill_listBox1();
+        }
+        string[] ksiazkidolisty = new string[100];
+        string[] info = new string[100];
+        void fill_listBox1()
+        {
+            Book myClass = new Book();
+            ksiazkidolisty = myClass.zapelnijcombobox();
+            int licznik = 0;
+            while (ksiazkidolisty[licznik] != null)
+            {
+                listBox1.Items.Add(ksiazkidolisty[licznik]);
+
+
+                licznik++;
+
+            }
+            licznik = 0;
         }
         string[] names = new string[100];
         string[] ksiazki = new string[100];
@@ -219,7 +237,35 @@ namespace Projekt_POiG
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            listBox2.Items.Clear();
+            int id = 0;
+
+            id = listBox1.SelectedIndex;
+            int index = ksiazkidolisty[id].IndexOf(":");
+            string poszukiwanystring = ksiazkidolisty[id];
+            string tytul = "";
+            int i = 0;
+            index++;
+            foreach (char s in poszukiwanystring)
+            {
+                if (i >= index)
+                {
+                    tytul += poszukiwanystring[i];
+                }
+                i++;
+
+            }
+            Book b1 = new Book();
+            info = b1.zapelnijlistbox(tytul);
+            for (i = 0; i <= 5; i++)
+            {
+                listBox2.Items.Add(info[i]);
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
